1d26edbf0a52abe70ef62ecd0b985de6a825bffa
sbtrd <66923353+sbtrd@users.noreply.github.com>
2026-07-30 18:56:30 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const VERSION = 'conduct-home-v1.71-clean';
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((names) => Promise.all(names.map((name) => caches.delete(name)))).then(() => self.clients.claim()));
-});
