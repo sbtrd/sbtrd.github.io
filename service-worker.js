f4d69df7e2ef9a5ab08b15819e4f8993596719a9
sbtrd <66923353+sbtrd@users.noreply.github.com>
2026-07-31 15:44:10 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'conduct-home-v1.85-calendrier';
+const VERSION = 'conduct-home-v1.86-dashboard-calendrier';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then((names) => Promise.all(names.map((name) => caches.delete(name)))).then(() => self.clients.claim()));
