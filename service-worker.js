874046e0aa0a20e1f344cd354ed30437d2a79a02
sbtrd <66923353+sbtrd@users.noreply.github.com>
2026-07-31 08:47:03 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'conduct-home-v1.77-mails-commandes';
+const VERSION = 'conduct-home-v1.78-outlook-choice';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then((names) => Promise.all(names.map((name) => caches.delete(name)))).then(() => self.clients.claim()));
