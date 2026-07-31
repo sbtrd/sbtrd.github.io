4a50589b9aa17fe7ec32dc0ea9dfa4f9c8666fa6
sbtrd <66923353+sbtrd@users.noreply.github.com>
2026-07-31 16:06:11 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'conduct-home-v1.87-duree-rendez-vous';
+const VERSION = 'conduct-home-v1.88-style-calendrier-iphone';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then((names) => Promise.all(names.map((name) => caches.delete(name)))).then(() => self.clients.claim()));
