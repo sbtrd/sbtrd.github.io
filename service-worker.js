38d4b97f8168fb3735a0c770abb3cba84eeea7c8
sbtrd <66923353+sbtrd@users.noreply.github.com>
2026-07-31 12:03:58 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'conduct-home-v1.83-signature-semaine-adaptative';
+const VERSION = 'conduct-home-v1.84-semaines-planning-artisan';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then((names) => Promise.all(names.map((name) => caches.delete(name)))).then(() => self.clients.claim()));
