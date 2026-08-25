c9aff0063528585190d6d4351d1156dad252387f
sbtrd <66923353+sbtrd@users.noreply.github.com>
2026-08-25 16:16:42 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const VERSION = 'conduct-home-v1.88-style-calendrier-iphone';
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((names) => Promise.all(names.map((name) => caches.delete(name)))).then(() => self.clients.claim()));
-});
